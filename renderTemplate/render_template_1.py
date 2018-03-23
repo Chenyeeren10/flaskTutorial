7400d7aae69003bfe95996eaf1cadfdc1087f131
ddx <dai.tianle@dhs.sg>
2018-03-24 01:36:42 +0800

    Update render_template_1.py

--- renderTemplate/render_template_1.py
+++ renderTemplate/render_template_1.py
@@ -2,4 +2,4 @@ from flask import Flask, render_template
 app = Flask(__name__) 
 @app.route('/hello/<int:score>')
 def hello_name(score):
-	return render_template('hello.html', marks=score)
+	return render_template('render_template_1.html', marks=score)
