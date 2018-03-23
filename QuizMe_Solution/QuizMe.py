f8f71277c6787c0ff417b5dc6c71c63e80639a8c
ddx <dai.tianle@dhs.sg>
2018-03-24 01:43:22 +0800

    Create QuizMe.py

---
+++ QuizMe_Solution/QuizMe.py
@@ -0,0 +1,11 @@
+from flask import Flask, request,render_template
+app = Flask(__name__)
+@app.route('/')
+def student():
+	return render_template('index.html')
+
+@app.route('/result',methods=['POST', 'GET'])
+def result():
+    if request.method=='POST':
+        result = request.form
+        return render_template("result.html",result = result)
