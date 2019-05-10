84385f48ac32d3a3f5a1f44992fdb96807692d51
Chenyeeren10 <48347987+Chenyeeren10@users.noreply.github.com>
2019-05-10 17:16:41 +0800

    Update app.py

--- More and more/Noteme_Templates/app.py
+++ More and more/Noteme_Templates/app.py
@@ -5,11 +5,14 @@ app.config["DEBUG"] = True
 
 user_comments = {}
 
-@app.route("/", methods=[     ,      ])  # hint: what is the methods we may use to differentiate two status?
+@app.route("/", methods=["GET", "POST"])  # hint: what is the methods we may use to differentiate two status?
 
 def index():
-    if request.method == "   ":  # hint: what if the user does not click the post button?
-        return render_template("", user_comments=user_comments)  # hint:what is the name for your main page?
+    if request.method == "GET":  # hint: what if the user does not click the post button?
+        return render_template("/", user_comments=user_comments)  # hint:what is the name for your main page?
     else: # post
         user_comments[request.form["uname"]] = request.form["contents"]
-        return redirect(                )   # hint: if we want to go back to index function, what should we do inside the redirect?
+        return redirect("/")   # hint: if we want to go back to index function, what should we do inside the redirect?
+File "/app/app.py"
+app.run(port=5050) - runs app
+
