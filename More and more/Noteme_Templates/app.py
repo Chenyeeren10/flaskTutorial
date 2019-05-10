0574c797a07c472e2741e204a76a7ed57131e269
Chenyeeren10 <48347987+Chenyeeren10@users.noreply.github.com>
2019-05-10 17:43:58 +0800

    Update app.py

--- More and more/Noteme_Templates/app.py
+++ More and more/Noteme_Templates/app.py
@@ -12,7 +12,7 @@ def index():
         return render_template("/", user_comments=user_comments)  # hint:what is the name for your main page?
     else: # post
         user_comments[request.form["uname"]] = request.form["contents"]
-        return redirect("/")   # hint: if we want to go back to index function, what should we do inside the redirect?
-File "/app/app.py"
+        return redirect(url_for("index"))   # hint: if we want to go back to index function, what should we do inside the redirect?
+if_name_=="main"
 app.run(port=5050) 
 
